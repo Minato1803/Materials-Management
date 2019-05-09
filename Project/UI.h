3dcf543d1c7adc0521ad5c1db6cdce98e52beed6
William <tncnhan@gmail.com>
2019-05-09 11:20:23 +0700

    x

--- Project/UI.h
+++ Project/UI.h
@@ -1,28 +1,15 @@
-//#include "create_structure.h"
+#define WD_WIDTH 1024
+#define WD_HEIGHT 600
+#define MAX_MENU 5
+#define DIS_MENUX 20 //khoang cach giua 2 muc menu
+#define DIS_MENUY 20
 
-//Cac ham ve
-void KhoiTaoChuongTrinh();
-void TaoManHinhLamViec();
-void VeMenu();
-void VeMenuPhu(int x, char menu[][30]);
-void NoiBatMuc(int x, int y, char menu[][30], int HL_COLOR, int C_SIZE);
-
-// Cac ham Menu
-void Menu();
-void MenuPhu(int viTriMenuPhu, char menu[][30], int MAXMENU, int &chonMuc);
-void ChonMenuPhu(int x);
-void Materials();
-void Employees();
-void Show();
-void Statistics();
-void Help();
-void About();
-void Guild();
-
-// Ham Vat Tu
-//void AddMat(NODEPTR &cayVatTu);
-void VeKhung2(char khungNoiDung[][30], int H, int W, int mucChon, int soLuongKhung);
-void veNut2(char khungNoiDung[][30], int ViTriKhung[], int SoLuongKhung, int H, int W, int mauNut, int kichThuocNut, int mucChon);
-void veKhung(int x, int y, char noiDung[], bool daChon, int mauNen, int mauChu);
-void veMuc2Chon(int x, int y, char noiDung[], char noiDungMucChon[][30], bool chonTrai, int mauNen, int mauChu);
-
+char MenuChinh[MAX_MENU+1][30] = {"Materials", "Employees", "Show", "Statistics", "Help"};
+char MenuMater[][30] = {"Add", "Remove", "Adjust"};
+char MenuEmp[][30] = {"Add", "Remove", "Adjust"};
+char MenuShow[][30] = {"Materials List", "Employees List", "Bills"};
+char MenuStat[][30] = {"Bills", "Top Materials"};
+char MenuHelp[][30] = {"Dell co gi ca!"};
+int ViTriMucMenu[MAX_MENU+1] = {10};
+int ViTriMucMenuPhu[5] = {0, 30, 60, 90, 120};
+int KichThuocMenuPhu[MAX_MENU+1] = {120, 120, 200, 180, 190};
